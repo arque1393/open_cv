23cddbe685ddca97cf307de5c6aca5943e2e4f1e
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-09-17 21:51:56 +0530

    colour conversion

--- image_handle.py
+++ image_handle.py
@@ -11,7 +11,17 @@ while True:
         cv2.destroyAllWindows()
         break
 
+cv2.destroyAllWindows()
 # In[2]:
+# Rescale Frame
+
+
+def rescaleFrame(frame, scale=.75):
+    height = frame.shape[0]
+    width = frame.shape[1]
+    return cv2.resize(frame, (width, height),
+                      interpolation=cv2.INTER_AREA)
+# In[3]:
 # Capture video from file
 capture = cv2.VideoCapture("./Resources/Videos/dog.mp4")
 # This function can take device no to capture video directly from camera device
@@ -26,18 +36,8 @@ try:
 except:
     pass
 capture.release()
-cv2.destroyAllWindows()
-# In[3]:
-# Rescale Frame
 
-
-def rescaleFrame(frame, scale=.75):
-    height = frame.shape[0]
-    width = frame.shape[1]
-    return cv2.resize(frame, (width, height),
-                      interpolation=cv2.INTER_AREA)
-
-# %%
+# In[4]:
 
 
 # Read in an image
@@ -78,7 +78,7 @@ while cv2.waitKey(0) != ord('j'):
 
 cv2.destroyAllWindows()
 
-# %%
+# In[5]:
 
 img = cv2.imread('./Resources/Photos/cats.jpg')
 cv2.imshow('Cats', img)
@@ -89,19 +89,20 @@ cv2.imshow('Blank', blank)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Gray', gray)
 
-blur = cv2.GaussianBlur(gray, (5,5), cv2.BORDER_DEFAULT)
+blur = cv2.GaussianBlur(gray, (5, 5), cv2.BORDER_DEFAULT)
 cv2.imshow('Blur', blur)
 
 canny = cv2.Canny(blur, 125, 175)
 cv2.imshow('Canny Edges', canny)
 
-# ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
-# cv.imshow('Thresh', thresh)
+# ret, thresh = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY)
+# cv2.imshow('Thresh', thresh)
 
-contours, hierarchies = cv2.findContours(canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchies = cv2.findContours(
+    canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 print(f'{len(contours)} contour(s) found!')
 
-cv2.drawContours(blank, contours, -1, (0,0,255), 1)
+cv2.drawContours(blank, contours, -1, (0, 0, 255), 1)
 cv2.imshow('Contours Drawn', blank)
 
 while cv2.waitKey(0) != ord('j'):
@@ -109,4 +110,43 @@ while cv2.waitKey(0) != ord('j'):
 
 cv2.destroyAllWindows()
 
-j# %%
+# In[6]:
+
+
+img = cv2.imread('./Resources/Photos/park.jpg')
+cv2.imshow('Park', img)
+
+# plt.imshow(img)
+# plt.show()
+
+# BGR to Grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Gray', gray)
+
+# BGR to HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cv2.imshow('HSV', hsv)
+
+# BGR to L*a*b
+lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+cv2.imshow('LAB', lab)
+
+# BGR to RGB
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow('RGB', rgb)
+
+# HSV to BGR
+lab_bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+cv2.imshow('LAB --> BGR', lab_bgr)
+
+while cv2.waitKey(0) != ord('j'):
+    pass
+
+cv2.destroyAllWindows()
+# In[7]:
+# Bitwise Operator 
+
+blank = np.zeros((400,400), dtype= 'uint8')
+rectangle = cv2.rectangle
+
+
